8df0a689e2a39582eef260176c98215f4836d68d
安志钢 <AnZhg@AnZhg-MacBook-Pro.local>
2016-10-13 15:38:07 -0400

    Initial Commit

---
+++ fft/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  fft
+//
+//  Created by 安志钢 on 16-10-13.
+//  Copyright (c) 2016年 安志钢. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
